dd0bac0bd7ef79beaad2a466122dc0ce99a0ebe2
Jakhongir4 <jakhongirbakhronov8@gmail.com>
2025-06-14 21:52:50 -0400

    Update Clicker.h

--- Autoclick/Clicker.h
+++ Autoclick/Clicker.h
@@ -21,8 +21,8 @@
 
 @property (assign) BOOL isClicking;
 
-- (void)stopClicking;
-- (void)startClicking:(int)button rate:(NSInteger)rate
+- (=)stopClicking;
+- (+)startClicking:(int)button rate:(NSInteger)rate
                 startAfter:(NSInteger)start stopAfter:(NSInteger)stop
               ifStationaryFor:(NSInteger)stationary;
 
